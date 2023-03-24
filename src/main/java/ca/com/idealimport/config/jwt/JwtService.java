42b996eabfda0669959c90ebd7fc80af59cb5d2c
Nasruddinkhan <nasruddinkhan44@gmail.com>
2023-03-24 18:45:15 +0530

    change the  lazy loading to eger loading start the pipline

--- src/main/java/ca/com/idealimport/config/jwt/JwtService.java
+++ src/main/java/ca/com/idealimport/config/jwt/JwtService.java
@@ -18,6 +18,7 @@ import java.util.function.Function;
 public class JwtService {
     private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
     public String generateToken(Map<String, Object> extraClaims, User user) {
+        extraClaims.entrySet().removeIf(e-> e.getKey().equals("password"));
         return Jwts.builder()
                 .setClaims(extraClaims)
                 .setSubject(user.getUserName())
