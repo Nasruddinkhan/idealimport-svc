87203a01690fb5401c5ab7f5e67bde2037690abd
Nasruddinkhan <nasruddinkhan44@gmail.com>
2023-05-06 15:21:49 +0530

    add generic card to show on mobile device

--- src/main/java/ca/com/idealimport/config/filter/builder/IdealResponseBuilder.java
+++ src/main/java/ca/com/idealimport/config/filter/builder/IdealResponseBuilder.java
@@ -62,7 +62,7 @@ public class IdealResponseBuilder {
 
     public void addResponseHeaders(HttpServletRequest request, HttpServletResponse response) {
         var interactionId = request.getHeader("interaction-id");
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
