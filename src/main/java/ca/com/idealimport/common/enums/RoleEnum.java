d96106ba270e78bb03019bc13f9ae67b233116f3
Nasruddinkhan <nasruddinkhan44@gmail.com>
2024-06-04 23:14:59 +0300

    add the role mapping and hide the menu based on role

---
+++ src/main/java/ca/com/idealimport/common/enums/RoleEnum.java
@@ -0,0 +1,13 @@
+package ca.com.idealimport.common.enums;
+
+public enum RoleEnum {
+    ADMIN,
+    USER,
+    INVENTORY_MANAGER,
+    SUPPLIER,
+    DELIVERY,
+    GUEST,
+    SUPER_ADMIN,
+    READ_USER,
+    CUSTOMER
+}
