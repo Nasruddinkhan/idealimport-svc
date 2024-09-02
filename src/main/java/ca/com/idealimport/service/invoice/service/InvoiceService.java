cf7a067bf070b107ee69fbe4149a7dd55c990dd7
Nasruddinkhan <nasruddinkhan44@gmail.com>
2024-09-03 00:15:25 +0300

    transfom the get order form list

--- src/main/java/ca/com/idealimport/service/invoice/service/InvoiceService.java
+++ src/main/java/ca/com/idealimport/service/invoice/service/InvoiceService.java
@@ -1,10 +1,15 @@
 package ca.com.idealimport.service.invoice.service;
 
+import ca.com.idealimport.service.invoice.model.SOOrderForm;
 import net.sf.jasperreports.engine.JRException;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface InvoiceService {
 
     byte[] createInvoice(String orderId) throws IOException, JRException;
+
+    List<SOOrderForm> createOrderInvoice(String orderId) throws IOException, JRException;
+
 }
